9d644bb23a452b3ef602f09e58a99342e522a123
tahira489 <musfikaanumtahira10@gmail.com>
2025-07-01 08:33:37 +0100

    Add files via upload

--- project3.py
+++ project3.py
@@ -1,13 +1,18 @@
-def reverse_bits(n, total_bits=32):
+def reverse_used_bits(n):
     result = 0
-    for i in range(total_bits):
-        result <<= 1
-        result |= (n & 1)
-        n >>= 1
+    num_bits = n.bit_length()  
+
+    for _ in range(num_bits):
+        result <<= 1         
+        result |= (n & 1)     
+        n >>= 1              
+
     return result
 
-number = 5  
-reversed_number = reverse_bits(number)
+number = 13 
+reversed_number = reverse_used_bits(number)
+
 print("Original number:", number)
+print("Original binary:", bin(number)[2:])
+print("Reversed binary:", bin(reversed_number)[2:])
 print("Reversed number:", reversed_number)
-print("Reversed binary:", bin(reversed_number))
